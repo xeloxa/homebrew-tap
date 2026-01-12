8a6306b76e4c9e85b64f5dd6c8231c097fa9eeba
Ali Sünbül <alisunbul@proton.me>
2026-01-12 20:20:23 +0300

    chore: Update s3finder to v1.0.2

--- Formula/s3finder.rb
+++ Formula/s3finder.rb
@@ -1,26 +1,26 @@
 class S3finder < Formula
   desc "AI-Powered S3 Bucket Enumeration Tool"
   homepage "https://github.com/xeloxa/s3finder"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.1/s3finder-v1.0.1-darwin-arm64.tar.gz"
-      sha256 "de5374f171ccd2b09dd05ec3a9bb007e03165f14ae79f80532d58964c03744ee"
+      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.2/s3finder-v1.0.2-darwin-arm64.tar.gz"
+      sha256 "416c5e8abc2ca1ff072af32e76b43d2d4e5fb4e11b13a887f9ed5b03241651c8"
     else
-      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.1/s3finder-v1.0.1-darwin-amd64.tar.gz"
-      sha256 "6b0a23ac8480906b6d0603ce80553ed3cbc961504bad1c73ac9df78d30f7b6b4"
+      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.2/s3finder-v1.0.2-darwin-amd64.tar.gz"
+      sha256 "3a6a8ac53f8938c482d46ee922d1459f7e12e2c4df829edb8290c5559fe0a351"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.1/s3finder-v1.0.1-linux-arm64.tar.gz"
-      sha256 "ea44e890b84970571222375c35f53d04d6bbda98fdb45a9048edadc514be1bbf"
+      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.2/s3finder-v1.0.2-linux-arm64.tar.gz"
+      sha256 "ef374c28223547981c40708cd4d5655cd55234eed40dd9c1d73e83fbfcdfe7a0"
     else
-      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.1/s3finder-v1.0.1-linux-amd64.tar.gz"
-      sha256 "1fed13caacabf877cb2f3bb9f3874342c72f64a8b00c7bac0359112f3b723906"
+      url "https://github.com/xeloxa/s3finder/releases/download/v1.0.2/s3finder-v1.0.2-linux-amd64.tar.gz"
+      sha256 "d2f42fc6ef14711749ed0424404fa10121e72d8f11b30f6af7a06d7130fd6a8a"
     end
   end
 
